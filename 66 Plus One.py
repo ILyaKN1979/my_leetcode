8f9d4d655a86f9a154df76fe1a9998d2ca1c091a
ILyaKN1979 <ilyakalagin@gmail.com>
2023-04-30 10:06:45 +0700

    add task number 66

---
+++ 66 Plus One.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 18:52:11 2022
+
+@author: Ilya
+"""
+
+digits = [9]
+
+num= ''.join(map(str, digits))
+
+res= int(num)+1
+
+print([a for a in str(res)])
