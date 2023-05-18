a55720acc990d41229bf7b2a798ff795242fdebd
Ilya Kalagin <ilyakalagin@gmail.com>
2023-05-18 21:23:12 +0700

    Add 1557. Minimum Number of Vertices to Reach All Nodes

---
+++ 1557. Minimum Number of Vertices to Reach All Nodes.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 19:22:35 2023
+Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] 
+represents a directed edge from node fromi to node toi.
+
+Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique 
+solution exists.
+
+Notice that you can return the vertices in any order.
+
+
+Examples:
+Input: n = 6, edges = [[0,1],[0,2],[2,5],[3,4],[4,2]]
+Output: [0,3]
+Explanation: It's not possible to reach all the nodes from a single vertex. 
+From 0 we can reach [0,1,2,5]. From 3 we can reach [3,4,2,5]. So we output [0,3].
+
+Input: n = 5, edges = [[0,1],[2,1],[3,1],[1,4],[2,4]]
+Output: [0,2,3]
+Explanation: Notice that vertices 0, 3 and 2 are not reachable from any other node, so we must include them. Also any of these vertices can reach nodes 1 and 4.
+
+@author: IlYA
+"""
+#input 
+n = 5
+edges = [[1,3],[2,0],[2,3],[1,0],[4,1],[0,3]]
+
+res = []
+second_elements = set([item[1] for item in edges])
+
+for i in range(0, n):
+    fl=0
+    if i in second_elements:
+        fl = 1
+    if fl==0:
+        res.append(i)
+   
+print(res)
